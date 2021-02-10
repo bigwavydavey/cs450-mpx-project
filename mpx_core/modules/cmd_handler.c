55eb04a2f0980b0eb2ae42dca9648cba8e639858
bigwavydavey <54442269+bigwavydavey@users.noreply.github.com>
2021-02-10 16:19:15 -0500

    Made cmd_handler its own file

--- mpx_core/modules/cmd_handler.c
+++ mpx_core/modules/cmd_handler.c
@@ -2,19 +2,31 @@
 #include <core/serial.h>
 #include <core/io.h>
 
+/**
+  Procedure..: cmd_handler
+  Description..: This function has a loop to continuously handle specific
+  user commands. As commands increase in quantity and complexity this
+  function will eventually call a host of other functions to handle
+  tasks.
+    Currently implemented commands:
+    --help
+    --version
+    --shutdown
+    --settime
+  Params..: none
+*/
 void cmd_handler()
 {
   char cmd_buffer[100];
   int buffer_size;
   int quit = 0;
 
-  char * startup_msg = "\nWelcome to OS Allstars' MPX. Enter help for a list of commands.\n";
+  char startup_msg[100] = "\nWelcome to OS Allstars' MPX. Enter help for a list of commands.\n";
   sys_req(WRITE, DEFAULT_DEVICE, startup_msg, &buffer_size);
 
- 
+
   while(!quit)
   {
-
     //Fill cmd_buffer
     sys_req(WRITE, DEFAULT_DEVICE, "\n>>", &buffer_size);
     memset(cmd_buffer, '\0', 100);
@@ -62,8 +74,11 @@ void cmd_handler()
       sys_req(WRITE, DEFAULT_DEVICE, help_msg, &buffer_size);
     }
 
-    //settime command
-    
+    /*
+    * This function is used to set the processors RTC's current time
+    *
+    * @param
+    */
     else if (strcmp(cmd_buffer, "settime\r") == 0)
     {
       int hr, min, sec;
@@ -75,7 +90,7 @@ void cmd_handler()
       int time_buffer_size = 8;
       sys_req(READ, DEFAULT_DEVICE, time_buffer, &time_buffer_size);
 
-      if (strlen(time_buffer) > 11)
+      if (strlen(time_buffer) > 9)
         sys_req(WRITE, DEFAULT_DEVICE, "\nInvalid format. Aborting settime command...\n", &buffer_size);
       else
       {
@@ -106,10 +121,16 @@ void cmd_handler()
           sti();
         }
 
-        sys_req(WRITE, DEFAULT_DEVICE, "\nTime succesfully updated\n", &buffer_size);
+        sys_req(WRITE, DEFAULT_DEVICE, "\nNew time succesfully set to register.\n", &buffer_size);
       }
     }
-
+    
+    /*
+    * This function is used to get the processors RTC's current time and print
+    * it to the window
+    *
+    * @param
+    */
     else if (strcmp(cmd_buffer, "gettime\r") == 0)
     {
       int hr_BCD, min_BCD, sec_BCD;
@@ -138,10 +159,120 @@ void cmd_handler()
       strcat(time, ":");
       strcat(time, sec_s);
 
+      sys_req(WRITE, DEFAULT_DEVICE, "The time is: ", &buffer_size);
       sys_req(WRITE, DEFAULT_DEVICE, time, &buffer_size);
+    }
+
+    /*
+    * This function is used to set the processors RTC's current date
+    *
+    * @param
+    */
+    else if (strcmp(cmd_buffer, "setdate\r") == 0)
+    {
+      int year_dec, year_mil, month, day;
+
+      sys_req(WRITE, DEFAULT_DEVICE, "Please enter time in MM/DD/YYYY format\n", &buffer_size);
+
+      char date_buffer[11];
+      memset(date_buffer, '\0', 11);
+      int date_buffer_size = 10;
+      sys_req(READ, DEFAULT_DEVICE, date_buffer, &date_buffer_size);
+
+      if (strlen(date_buffer) > 11)
+        sys_req(WRITE, DEFAULT_DEVICE, "\nInvalid format. Aborting setdate command...\n", &buffer_size);
+      else
+      {
+        char * month_s = strtok(date_buffer, "/");
+        char * day_s = strtok(NULL, "/");
+        char * year_s_init = strtok(NULL, "/");
+        char year_mil_s[3] = {year_s_init[0], year_s_init[1], '\0'};
+        char year_dec_s[3] = {year_s_init[2], year_s_init[3], '\0'};
+
+        month = atoi(month_s);
+        day = atoi(day_s);
+        year_mil = atoi(year_mil_s);
+        year_dec = atoi(year_dec_s);
+
+        unsigned int month_BCD = (((month / 10) << 4) | (month % 10));
+        unsigned int day_BCD = (((day / 10) << 4) | (day % 10));
+        unsigned int year_mil_BCD = (((year_mil / 10) << 4) | (year_mil % 10));
+        unsigned int year_dec_BCD = (((year_dec / 10) << 4) | (year_dec % 10));
+
+        if( (month <= 0) | (month > 12) | (day <= 0) | (day > 31) )
+          sys_req(WRITE, DEFAULT_DEVICE, "\nInvalid numerical input. Aborting settime command...\n", &buffer_size);
+        else if( (month == 2) && (day > 29) )
+          sys_req(WRITE, DEFAULT_DEVICE, "\nInvalid numerical input. Aborting settime command...\n", &buffer_size);
+        else
+        {
+          cli();
+          outb(0x70, 0x08);
+          outb(0x71, month_BCD);
+          outb(0x70, 0x07);
+          outb(0x71, day_BCD);
+          outb(0x70, 0x32);
+          outb(0x71, year_mil_BCD);
+          outb(0x70, 0x09);
+          outb(0x71, year_dec_BCD);
+          sti();
+        }
+
+        sys_req(WRITE, DEFAULT_DEVICE, "\nNew date succesfully set to register.\n", &buffer_size);
+      }
+    }
+
+    /*
+    * This function is used to get the processors RTC's current date and print
+    * it to the window
+    *
+    * @param
+    */
+    else if (strcmp(cmd_buffer, "getdate\r") == 0)
+    {
+      int month_BCD, day_BCD, year_mil_BCD, year_dec_BCD;
+      char date[11] = "\0";
+
+      outb(0x70, 0x08);
+      month_BCD = inb(0x71);
+      outb(0x70, 0x07);
+      day_BCD = inb(0x71);
+      outb(0x70, 0x32);
+      year_mil_BCD = inb(0x71);
+      outb(0x70, 0x09);
+      year_dec_BCD = inb(0x71);
+
+      int month = (((month_BCD >> 4) * 10) + (month_BCD & 0xF));
+      char month_s[3];
+      itoa(month, month_s, 10);
+      int day = (((day_BCD >> 4) * 10) + (day_BCD & 0xF));
+      char day_s[3];
+      itoa(day, day_s, 10);
+      int year_mil = (((year_mil_BCD >> 4) * 10) + (year_mil_BCD & 0xF));
+      char year_mil_s[3];
+      itoa(year_mil, year_mil_s, 10);
+      int year_dec = (((year_dec_BCD >> 4) * 10) + (year_dec_BCD & 0xF));
+      char year_dec_s[3] = "\0";
+      if(year_dec < 10)
+      {
+        char year_dec_temp[3] = "/0";
+        strcat(year_dec_s, "0");
+        strcat(year_dec_s, itoa(year_dec, year_dec_temp, 10));
+      }
+      else
+        itoa(year_dec, year_dec_s, 10);
+
+      strcat(date, month_s);
+      strcat(date, "/");
+      strcat(date, day_s);
+      strcat(date, "/");
+      strcat(date, year_mil_s);
+      strcat(date, year_dec_s);
+
+      sys_req(WRITE, DEFAULT_DEVICE, "The date is: ", &buffer_size);
+      sys_req(WRITE, DEFAULT_DEVICE, date, &buffer_size);
 
     }
-    
+
     //Command not recognized
     else
     {
