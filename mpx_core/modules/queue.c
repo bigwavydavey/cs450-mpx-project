92972a57a7c66878e4a4ae6813aa743b6ffe4dbe
Nathan <nathan@loud-2020>
2021-02-19 09:32:51 -0500

    added pcb struct

--- mpx_core/modules/queue.c
+++ mpx_core/modules/queue.c
@@ -7,3 +7,17 @@ struct queue
   pcb *head;
   pcb *tail;
 }
+
+struct pcb
+{
+	char[100] name;
+	char[100] class;
+	int priority;
+	// 0- ready not suspended, 1- ready suspended, 2- blocked non suspended, 3- blcoked suspended, 5- running
+	int state;
+	unsigned char[1024] stack;
+	unsigned char* top;
+	unsigned char* base;
+	pcb *next;
+	pcb *previous;
+}
