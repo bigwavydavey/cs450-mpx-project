a501fba57ed3ae7c29ad26b14393ccb51e13419a
David Maxson <david@loud-2020>
2021-02-26 07:43:22 -0500

    Updated help again

--- mpx_core/modules/cmd_handler.c
+++ mpx_core/modules/cmd_handler.c
@@ -313,28 +313,30 @@ void optional_cmd_handler(char * cmd_buffer)
 */
 void help()
 {
-      //R1 commands
-      sys_req(WRITE, DEFAULT_DEVICE, "\nhelp: prints list of commands and explains their functionality\n", &buffer_size);
-      sys_req(WRITE, DEFAULT_DEVICE, "\nversion: prints the current version of OS Allstars' MPX and most recent\nrelease date\n", &buffer_size);
-      sys_req(WRITE, DEFAULT_DEVICE,  "\nshutdown: shutsdown the MPX system. You will be asked for confirmation\n", &buffer_size);
-      sys_req(WRITE, DEFAULT_DEVICE, "\ngetdate: prints current date as stored in MPX register\n", &buffer_size);
-      sys_req(WRITE, DEFAULT_DEVICE, "\nsetdate: sets a user input date to the register\n", &buffer_size);
-      sys_req(WRITE, DEFAULT_DEVICE, "\ngettime: prints the current time of day as stored in MPX register\n", &buffer_size);
-      sys_req(WRITE, DEFAULT_DEVICE, "\nsettime: sets a user input time of day to the register\n", &buffer_size);
+  buffer_size = 700;
 
-      //R2 commands
-      sys_req(WRITE, DEFAULT_DEVICE, "\ncreatepcb [pcb_name] [pcb_class] [pcb_priority]: This command will create a new PCB for the given attributes, pcb_name, pcb_priority, pcb_class.\n", &buffer_size);
-      sys_req(WRITE, DEFAULT_DEVICE, "\ndeleteocb [pcb_name]: This command will delete a selected pcb from all 4 of the PCB queues, removing them completely from the system. pcb_name must be a valid PCB already inexistence.\n", &buffer_size);
-      sys_req(WRITE, DEFAULT_DEVICE, "\nblockpcb [pcb_name]: This command will set the selected PCB’s state to blocked and insert it into the appropriate PCB queue. pcb_name must be a valid PCB already in existence. \n", &buffer_size);
-      sys_req(WRITE, DEFAULT_DEVICE, "\nunblockpcb [pcb_name]: This command will set the selected PCB’s state to unblocked and insert it in to theappropriate PCB queue. pcb_name must be a valid PCB already in existence. \n", &buffer_size);
-      sys_req(WRITE, DEFAULT_DEVICE, "\nsuspendpcb [pcb_name]: This command will set the selected PCB’s state to suspended and insert it in tothe appropriate PCB queue. pcb_name must be a valid PCB already in existence.  \n", &buffer_size);
-      sys_req(WRITE, DEFAULT_DEVICE, "\nresumepcb [pcb_name]: This command will set the selected PCB’s state to unsuspended and insert it in tothe appropriate PCB queue. pcb_name must be a valid PCB already in existence.  \n", &buffer_size);
-      sys_req(WRITE, DEFAULT_DEVICE, "\nsetpriority [pcb_name]: This command will set the selected PCB’s priority to a new user desired priority, possibly changing its location withing the queues. pcb_name and pcb_priority must both be valid\n", &buffer_size);
-      sys_req(WRITE, DEFAULT_DEVICE, "\nshowpcb [pcb_name]: This command will display the attributes of a selected PCB in the terminal. These attributes include process_name, class, state, suspended_status, and priority\n", &buffer_size);
-      sys_req(WRITE, DEFAULT_DEVICE, "\nshowreadypcb: This command will display all processes contained within the two ready queues in the terminal.\n", &buffer_size);
-      sys_req(WRITE, DEFAULT_DEVICE, "\nshowreadypcb: This command will display all processes contained within the two ready queues in the terminal.\n", &buffer_size);
-      sys_req(WRITE, DEFAULT_DEVICE, "\nshowblkpcb: This command will display all processes contained within the two blocked queues in the terminal.\n", &buffer_size);
-      sys_req(WRITE, DEFAULT_DEVICE, "\nshowpcbs: This commands will display all existing processes, in all four queues, in the terminal.\n", &buffer_size);
+  //R1 commands
+  sys_req(WRITE, DEFAULT_DEVICE, "\nhelp: prints list of commands and explains their functionality\n", &buffer_size);
+  sys_req(WRITE, DEFAULT_DEVICE, "\nversion: prints the current version of OS Allstars' MPX and most recent\nrelease date\n", &buffer_size);
+  sys_req(WRITE, DEFAULT_DEVICE,  "\nshutdown: shutsdown the MPX system. You will be asked for confirmation\n", &buffer_size);
+  sys_req(WRITE, DEFAULT_DEVICE, "\ngetdate: prints current date as stored in MPX register\n", &buffer_size);
+  sys_req(WRITE, DEFAULT_DEVICE, "\nsetdate: sets a user input date to the register\n", &buffer_size);
+  sys_req(WRITE, DEFAULT_DEVICE, "\ngettime: prints the current time of day as stored in MPX register\n", &buffer_size);
+  sys_req(WRITE, DEFAULT_DEVICE, "\nsettime: sets a user input time of day to the register\n", &buffer_size);
+
+  //R2 commands
+  sys_req(WRITE, DEFAULT_DEVICE, "\ncreatepcb [pcb_name] [pcb_class] [pcb_priority]: This command will create a new PCB for the given attributes, pcb_name, pcb_priority, pcb_class.\n", &buffer_size);
+  sys_req(WRITE, DEFAULT_DEVICE, "\ndeleteocb [pcb_name]: This command will delete a selected pcb from all 4 of the PCB queues, removing them completely from the system. pcb_name must be a valid PCB already in existence.\n", &buffer_size);
+  sys_req(WRITE, DEFAULT_DEVICE, "\nblockpcb [pcb_name]: This command will set the selected PCB’s state to blocked and insert it into the appropriate PCB queue. pcb_name must be a valid PCB already in existence. \n", &buffer_size);
+  sys_req(WRITE, DEFAULT_DEVICE, "\nunblockpcb [pcb_name]: This command will set the selected PCB’s state to unblocked and insert it in to theappropriate PCB queue. pcb_name must be valid. \n", &buffer_size);
+  sys_req(WRITE, DEFAULT_DEVICE, "\nsuspendpcb [pcb_name]: This command will set the selected PCB’s state to suspended and insert it in tothe appropriate PCB queue. pcb_name must be valid.  \n", &buffer_size);
+  sys_req(WRITE, DEFAULT_DEVICE, "\nresumepcb [pcb_name]: This command will set the selected PCB’s state to unsuspended and insert it in tothe appropriate PCB queue. pcb_name must be valid.  \n", &buffer_size);
+  sys_req(WRITE, DEFAULT_DEVICE, "\nsetpriority [pcb_name]: This command will set the selected PCB’s priority to a new user desired priority,\npossibly changing its location withing the queues. pcb_name and pcb_priority must be valid\n", &buffer_size);
+  sys_req(WRITE, DEFAULT_DEVICE, "\nshowpcb [pcb_name]: This command will display the attributes of a selected PCB in the terminal.\nThese attributes include process_name, class, state, suspended_status, and priority\n", &buffer_size);
+  sys_req(WRITE, DEFAULT_DEVICE, "\nshowreadypcb: This command will display all processes contained within the two ready queues in the terminal.\n", &buffer_size);
+  sys_req(WRITE, DEFAULT_DEVICE, "\nshowreadypcb: This command will display all processes contained within the two ready queues in the terminal.\n", &buffer_size);
+  sys_req(WRITE, DEFAULT_DEVICE, "\nshowblkpcb: This command will display all processes contained within the two blocked queues in the terminal.\n", &buffer_size);
+  sys_req(WRITE, DEFAULT_DEVICE, "\nshowpcbs: This commands will display all existing processes, in all four queues, in the terminal.\n", &buffer_size);
 }
 /**
 
