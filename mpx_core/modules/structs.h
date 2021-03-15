b22796a3e6c8bb7d3671ae21ce71e1ce74b71179
Lennon Jones <lennon@loud-2020>
2021-03-15 13:19:03 -0400

    R3 User commands and addition of context struct

--- mpx_core/modules/structs.h
+++ mpx_core/modules/structs.h
@@ -22,4 +22,11 @@ struct pcb
 	struct pcb *prev;
 };
 
+struct context
+{
+	u32int gs, fs, es, ds;
+	u32int edi, esi, ebp, esp, ebx, edx, ecx, eax;
+	u32int eip, cs, eflags;
+};
+
 #endif
---
+++ mpx_core/modules/userR3Commands.h
@@ -0,0 +1,8 @@
+#ifndef _USERR3COMMANDS_H
+#define _USERR3COMMANDS_H value
+
+void yield();
+
+void loadr3();
+
+#endif
